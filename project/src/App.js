697642cc8cbdd623dd3f4273a52075cf2b0033e3
iinourah96@gmail.com <iinourah96@gmail.com>
2019-04-03 17:14:11 +0300

    add readme

---
+++ project/src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './Nav'
+import Recipe from './Recipe';
+import Star from './Star'
+// import Footer  from'./Footer'
+
+
+class App extends Component {
+  state={
+    recipeShow :[]
+  }
+
+  handleSearch=input =>{
+    const url = `https://www.food2fork.com/api/search?key=ae4094d32b86720d5ae572f751c926d0&q=${input}&count=27 `
+    fetch(url).then(response => {
+      response.json().then(data => {
+      console.log(data) // take a look at what you get back!
+        
+      // console.log();
+      
+        this.setState({recipeShow: data.recipes})
+      })
+      }
+    )
+  }
+
+  componentDidMount= () => {
+      // api key1 a66b740af013fda9a5b735cd25a08f14
+      // api key2 ae4094d32b86720d5ae572f751c926d0 
+      // api key3  903d1a52f97c8d79bb5008bdf73e8e48
+      // api key4 21fe3157728163bad920b76b86ffe824
+    const url = 'https://www.food2fork.com/api/search?key=ae4094d32b86720d5ae572f751c926d0   '
+    fetch(url).then(response => {
+      response.json().then(data => {
+      console.log(data) // take a look at what you get back!
+        
+      // console.log();
+      
+        this.setState({recipeShow: data.recipes})
+      })
+      }
+    )
+  }
+  render() {
+
+      const recipes= this.state.recipeShow.map(recipe=> {
+        return  <Recipe recipes ={recipe} />
+      })
+      console.log(recipes)
+      return (
+        
+        <div className="App">
+      <Nav search={this.handleSearch} />
+      {/* <Rating /> */}
+      {recipes}
+      {/* {test} */}
+      {/* <Star star="1"/>   */}
+        </div>
+      );
+  }
+}
+
+export default App;
